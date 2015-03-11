2d583a7520c52f9db56a18afa8bb6eaa2fd2dcaf
PJ <yorko9999@gmail.com>
2015-03-11 13:23:47 +0000

    Third commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -400,7 +400,7 @@ module.exports = function (grunt) {
     },
     heroku: {
       options: {
-        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        remote: 'git@heroku.com:evening-taiga-7782.git',
         branch: 'master',
         tag: pkg.version
       }
